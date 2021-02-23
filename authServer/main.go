0487e34e268054aaa9a81d54c9fff02b6fc5e5d8
pweber <4fd49ecc792958c1ed7495830435490f@patweber.de>
2021-02-23 14:55:26 +0100

    remove log with fatal

--- authServer/main.go
+++ authServer/main.go
@@ -150,7 +150,6 @@ func pubkeyHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	if (len(r.TLS.VerifiedChains) > 0 || basicAuth(w, r)) {
 		reqBody, _ := ioutil.ReadAll(r.Body)
-		glog.Fatalf(string(reqBody))
 		var req PubkeyRequest
 	json.Unmarshal(reqBody, &req)
 		user, ok := cfg.Users[req.Username]
