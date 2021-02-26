610f421c22ea569939b0ac11593cf3e7d44c4b0c
pweber <4fd49ecc792958c1ed7495830435490f@patweber.de>
2021-02-26 01:36:06 +0100

    play-docker

--- helper-scripts/docker-build-all.sh
+++ helper-scripts/docker-build-all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ex
+. helper-scripts/docker-image-prefix.sh
+export IMAGE_NAME="$(basename $IMAGE_NAME)"
 docker build -t $IMAGEPREFIX$IMAGE_NAME .
 pushd authServer
 docker build -t $IMAGE_PREFIXauthserver .
