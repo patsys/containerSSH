ce83a8432de149f2f9f9e851f78dbd17507a13ca
pweber <4fd49ecc792958c1ed7495830435490f@patweber.de>
2021-02-28 12:44:00 +0100

    play-docker

--- helper-scripts/docker-test-all.sh
+++ helper-scripts/docker-test-all.sh
@@ -1,40 +1,45 @@
 #!/bin/bash
-testSuccess=9
-set -ex
-. ./helper-scripts/docker-env-defaults.sh
-pushd authServer
-docker-compose -f docker-compose.test.yml -p authserver up -d
-ret="$(docker wait authserver_sut_1)"
-if [ $ret -ne 0 ]; then
-  echo "authserver test failed"
-  testSuccess=1
-else
-  echo "authserver test success"
-fi
-popd
-docker-compose -f docker-compose.test.yml -p authserver down
-pushd configServer
-docker-compose -f docker-compose.test.yml -p configserver up -d
-ret="$(docker wait configserver_sut_1)"
-if [ $ret -ne 0 ]; then
-  echo "configserver test failed"
-  testSuccess=1
-else
-  echo "configserver test success"
-fi
-docker-compose -f docker-compose.test.yml -p configserver down
-popd
-bash -c '
-sleep 5
-ret="$(docker wait containerssh_sut_1)"
-if [ $ret -ne 0 ]; then
-  echo "containerssh test failed"
-  testSuccess=1
-else
-  echo "containerssh test success"
-fi
-sleep 600
-docker-compose -f docker-compose.test.yml -p containerssh down 
-' &
-docker-compose -f docker-compose.test.yml -p containerssh up 
+function test {
+  pushd $1
+
+  ( for i in `seq 150` ; do
+    if [ $(docker ps -a -f NAME=$2_sut_1 | wc -l) -ge 2 ]; then
+      break;
+    fi
+    sleep 1
+  done
+  ret="$(docker wait $2_sut_1)"
+  sleep 1
+  docker-compose -f docker-compose.test.yml ps
+  docker-compose -f docker-compose.test.yml -p $2 down
+  exit $ret ) &
+
+  pid=$!
+  if [ "$DOCKER_TEST_VERBOSE" == true ]; then
+    docker-compose -f docker-compose.test.yml -p $2 up -V $DOCKER_COMPOSE_UP_PARAMS
+    wait $pid
+    ret=$?
+  else
+    docker-compose -f docker-compose.test.yml -p $2 up -V $DOCKER_COMPOSE_UP_PARAMS &>/tmp/$2.log
+    wait $pid
+    ret=$?
+    if [ "$ret" -ne 0 ]; then
+      cat /tmp/$2.log
+    fi
+  fi
+  popd
+  if [ $ret -ne 0 ]; then
+    echo "$2 test failed"
+    testSuccess=1
+    return 1
+  else
+    echo "containerssh test success"
+    return 0
+  fi
+  return $ret
+}
+testSuccess=0
+test authServer authserver
+test configServer configserver
+test . containerssh
 exit $testSuccess
