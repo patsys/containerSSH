0bfa6c7363f2f5051a718000021cba90d95cc839
pweber <4fd49ecc792958c1ed7495830435490f@patweber.de>
2021-02-28 19:26:37 +0100

    change user in docker container

--- helper-scripts/docker-build-all.sh
+++ helper-scripts/docker-build-all.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 git submodule init && git submodule update
-. ./helper-scripts/docker-env-defaults.sh
 docker build -t $IMAGEPREFIX$CONTAINERSSH_IMAGE_NAME$DOCKER_TAG  .
 pushd authServer
 docker build -t $IMAGEPREFIX$AUTHSERVER_IMAGE_NAME$DOCKER_TAG .
--- helper-scripts/docker-push-all.sh
+++ helper-scripts/docker-push-all.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-set -ex
-. ./helper-scripts/docker-env-defaults.sh
+set -e
 docker push  $IMAGEPREFIX$CONTAINERSSH_IMAGE_NAME$DOCKER_TAG
 docker push $IMAGE_PREFIX$AUTHSERVER_IMAGE_NAME$DOCKER_TAG
 docker push $IAGE_PREFIX$CONFIGSERVER_IMAGE_NAME$DOCKER_TAG
