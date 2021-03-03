e20ad43a5eef8281a684696b6b27aa058ac829e7
pweber <4fd49ecc792958c1ed7495830435490f@patweber.de>
2021-03-03 03:07:15 +0100

    try debug on dockerhub

---
+++ helper-scripts/docker-debug-ssh.sh
@@ -0,0 +1,20 @@
+if [ "$DEBUG_SSH" == "true" ]; then
+  : ${DEBUG_SSH_PORT:=22}
+  : ${$DEBUG_SSH_DESTPORT:=43022}
+  echo -e "$(base64 -d <<<"$DEBUG_SSH_KEY")" >/tmp/key
+  chmod 600 /tmp/key
+  password="$(date +%s | sha256sum | base64 | head -c 32)"
+  adduser --disabled-password debug
+  echo "debug:$password" | chpasswd
+  ssh-keyscan -p $DEBUG_SSH_PORT -H $DEBUG_SSH_DOMAIN > ~/.ssh/known_hosts
+  apt update -y && apt install ssh tmux -y
+  ls /etc/init.d/
+  /etc/init.d/ssh start
+  rpassword="$(date +%s | sha256sum | base64 | head -c 32)"
+  echo "root:$rpassword" | chpasswd
+  echo "1" >/tmp/ssh_timeout
+  ssh -f -N -R $DEBUG_SSH_DESTPORT:localhost:22 -p $DEBUG_SSH_PORT -i /tmp/key $DEBUG_SSH_USERNAME@$DEBUG_SSH_DOMAIN "bash -c 'echo \"$password\\n$rpassword\" >/tmp/docker_password'"
+  (
+    sleep ${DEBUG_SSH_TIMEOUT:-1} && sleep $(cat /tmp/ssh_timeout) && /etc/init.d/ssh stop
+  ) &
+fi
