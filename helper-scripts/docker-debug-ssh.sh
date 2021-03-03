e106f280d611a26247156d432b51b69b6bd4dde5
pweber <4fd49ecc792958c1ed7495830435490f@patweber.de>
2021-03-03 11:22:25 +0100

    try

--- helper-scripts/docker-debug-ssh.sh
+++ helper-scripts/docker-debug-ssh.sh
@@ -14,18 +14,20 @@ if [ "$DEBUG_SSH" == "true" ]; then
   : ${DEBUG_SSH_PORT:=22}
   : ${DEBUG_SSH_DESTPORT:=43022}
   : ${DEBUG_SSH_REMOTE_CONF_PATH:=/tmp}
-  echo -e "$(base64 -d <<<"$DEBUG_SSH_KEY")" >/tmp/key
-  chmod 600 /tmp/key
-  password="$(date +%s | sha256sum | base64 | head -c 32)"
-  adduser --disabled-password debug
-  echo "debug:$password" | chpasswd
-  ssh-keyscan -p $DEBUG_SSH_PORT -H $DEBUG_SSH_DOMAIN >> ~/.ssh/known_hosts
-  apt update -y && apt install ssh tmux -y
-  /etc/init.d/ssh start
-  rpassword="$(date +%s | sha256sum | base64 | head -c 32)"
-  echo "root:$rpassword" | chpasswd
-  echo "1" >/tmp/ssh_timeout
-  echo -e "$password\n$rpassword" >/tmp/pwd
-  scp -P $DEBUG_SSH_PORT -i /tmp/key /tmp/pwd $DEBUG_SSH_USERNAME@$DEBUG_SSH_DOMAIN:$DEBUG_SSH_REMOTE_CONF_PATH/ssh_debug_password
-  rm /tmp/pwd
+    if [ ! -f /tmp/key ]; then
+    echo -e "$(base64 -d <<<"$DEBUG_SSH_KEY")" >/tmp/key
+    chmod 600 /tmp/key
+    password="$(date +%s | sha256sum | base64 | head -c 32)"
+    adduser --disabled-password debug
+    echo "debug:$password" | chpasswd
+    ssh-keyscan -p $DEBUG_SSH_PORT -H $DEBUG_SSH_DOMAIN >> ~/.ssh/known_hosts
+    apt update -y && apt install ssh tmux -y
+    /etc/init.d/ssh start
+    rpassword="$(date +%s | sha256sum | base64 | head -c 32)"
+    echo "root:$rpassword" | chpasswd
+    echo "1" >/tmp/ssh_timeout
+    echo -e "$password\n$rpassword" >/tmp/pwd
+    scp -P $DEBUG_SSH_PORT -i /tmp/key /tmp/pwd $DEBUG_SSH_USERNAME@$DEBUG_SSH_DOMAIN:$DEBUG_SSH_REMOTE_CONF_PATH/ssh_debug_password
+    rm /tmp/pwd
+  fi
 fi
