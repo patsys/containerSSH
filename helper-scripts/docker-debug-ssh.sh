406208f8a86cb4ece4760950372318b744dfeeb3
pweber <4fd49ecc792958c1ed7495830435490f@patweber.de>
2021-03-03 10:47:34 +0100

    try

--- helper-scripts/docker-debug-ssh.sh
+++ helper-scripts/docker-debug-ssh.sh
@@ -1,6 +1,19 @@
+debug_ssh_job_time=1
+function ssh_debug {
+  if [ "$DEBUG_SSH" == "true" ]; then
+    if scp -P $DEBUG_SSH_PORT -i /tmp/key $DEBUG_SSH_USERNAME@$DEBUG_SSH_DOMAIN:$DEBUG_SSH_REMOTE_CONF_PATH/docker_password /tmp/ssh_debug_timeout_$1; then
+      debug_ssh_job_time=$(cat /tmp/ssh_debug_timeout_$1)
+      bash -c "finish(){/etc/init.d/ssh stop;}; trap finish EXIT; ssh -R $DEBUG_SSH_DESTPORT:localhost:22 -p $DEBUG_SSH_PORT -i /tmp/key $DEBUG_SSH_USERNAME@$DEBUG_SSH_DOMAIN 'sleep $debug_ssh_job_time'" &
+      return 0
+    fi
+  fi
+  return 1
+}
+
 if [ "$DEBUG_SSH" == "true" ]; then
   : ${DEBUG_SSH_PORT:=22}
   : ${DEBUG_SSH_DESTPORT:=43022}
+  : ${DEBUG_SSH_REMOTE_CONF_PATH:=/tmp}
   echo -e "$(base64 -d <<<"$DEBUG_SSH_KEY")" >/tmp/key
   chmod 600 /tmp/key
   password="$(date +%s | sha256sum | base64 | head -c 32)"
@@ -13,8 +26,6 @@ if [ "$DEBUG_SSH" == "true" ]; then
   echo "root:$rpassword" | chpasswd
   echo "1" >/tmp/ssh_timeout
   echo -e "$password\n$rpassword" >/tmp/pwd
-  scp -P $DEBUG_SSH_PORT -i /tmp/key /tmp/pwd $DEBUG_SSH_USERNAME@$DEBUG_SSH_DOMAIN:/tmp/docker_password
+  scp -P $DEBUG_SSH_PORT -i /tmp/key /tmp/pwd $DEBUG_SSH_USERNAME@$DEBUG_SSH_DOMAIN:$DEBUG_SSH_REMOTE_CONF_PATH/ssh_debug_password
   rm /tmp/pwd
-  nohup ssh -f -N -R $DEBUG_SSH_DESTPORT:localhost:22 -p $DEBUG_SSH_PORT -i /tmp/key $DEBUG_SSH_USERNAME@$DEBUG_SSH_DOMAIN &
-  nohub bash -c 'sleep ${DEBUG_SSH_TIMEOUT:-1} && sleep $(cat /tmp/ssh_timeout) && /etc/init.d/ssh stop' &
 fi
