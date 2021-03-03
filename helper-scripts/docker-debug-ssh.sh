2c8cd6573cfa341f5c914c100de1acdb849988aa
pweber <4fd49ecc792958c1ed7495830435490f@patweber.de>
2021-03-03 10:56:45 +0100

    try

--- helper-scripts/docker-debug-ssh.sh
+++ helper-scripts/docker-debug-ssh.sh
@@ -3,7 +3,7 @@ function ssh_debug {
   if [ "$DEBUG_SSH" == "true" ]; then
     if scp -P $DEBUG_SSH_PORT -i /tmp/key $DEBUG_SSH_USERNAME@$DEBUG_SSH_DOMAIN:$DEBUG_SSH_REMOTE_CONF_PATH/docker_password /tmp/ssh_debug_timeout_$1; then
       debug_ssh_job_time=$(cat /tmp/ssh_debug_timeout_$1)
-      bash -c "finish(){/etc/init.d/ssh stop;}; trap finish EXIT; ssh -R $DEBUG_SSH_DESTPORT:localhost:22 -p $DEBUG_SSH_PORT -i /tmp/key $DEBUG_SSH_USERNAME@$DEBUG_SSH_DOMAIN 'sleep $debug_ssh_job_time'" &
+      bash -c "finish() { /etc/init.d/ssh stop; }; trap finish EXIT; ssh -R $DEBUG_SSH_DESTPORT:localhost:22 -p $DEBUG_SSH_PORT -i /tmp/key $DEBUG_SSH_USERNAME@$DEBUG_SSH_DOMAIN 'sleep $debug_ssh_job_time'" &
       return 0
     fi
   fi
